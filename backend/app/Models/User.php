0bdf985410a322b848549974c946b9371bc978f7
NemethTamas1 <nemethtamas029@gmail.com>
2026-08-07 11:16:32 +0200

    Model updated for user

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,6 +7,8 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -14,19 +16,49 @@ use Illuminate\Notifications\Notifiable;
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    protected $table = "users";
+
+    public $timestamps = true;
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'renter_rating' => 'decimal:2',
+            'lender_rating' => 'decimal:2',
         ];
+    }
+
+    // Összes létrehozott eszközöm.
+    public function gears(): HasMany
+    {
+        return $this->hasMany(Gear::class, 'user_id');
+    }
+
+    // Általam bérelt eszközök.
+    public function rentalsAsRenter(): HasMany
+    {
+        return $this->hasMany(Rental::class, 'renter_id');
+    }   
+
+    // Bérbeadott eszközeim.
+    public function rentalsAsOwner(): HasManyThrough
+    {
+        return $this->hasManyThrough(Renatl::class, Gear::class, 'user_id', 'gear_id');
+    }
+
+    // Kiadott értékelési rekordok.
+    public function ratingsGiven(): HasMany
+    {
+        return $this->hasMany(Rating::class, 'rater_id');
+    }
+
+    // Kapott értékelési rekordok. 
+    public function ratingsReceieved(): HasMany
+    {
+        return $this->hasMany(Rating::class, 'rated_id');
     }
 }
