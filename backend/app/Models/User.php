34a7ccaedcabbce3241e1370149692d4c2eee5c1
NemethTamas1 <nemethtamas029@gmail.com>
2026-08-07 12:45:17 +0200

    usr fillable extended

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -12,7 +12,7 @@ use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'phone'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
