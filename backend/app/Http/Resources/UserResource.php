b585b8ba67ce80cd3715ac6a59e123cd78c425fd
NemethTamas1 <nemethtamas029@gmail.com>
2026-08-07 12:45:01 +0200

    resrource added to user model

---
+++ backend/app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "email" => $this->email,
+            "phone" => $this->phone,
+            "renter_rating" => $this->renter_rating,
+            "lender_rating" => $this->lender_rating,
+        ];
+    }
+}
