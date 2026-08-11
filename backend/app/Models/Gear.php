4657010a51c603f619bc0492bc1f34ceb4295d50
NemethTamas1 <nemethtamas029@gmail.com>
2026-08-11 13:59:05 +0200

    gear model added

---
+++ backend/app/Models/Gear.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+#[Fillable(['title', 'description', 'type', 'price_per_day', 'city', 'address'])]
+class Gear extends Model
+{
+    protected $table = "gears";
+
+    public $timestamps = true;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(GearImage::class, 'gear_id');
+    }
+
+    public function rentals(): HasMany
+    {
+        return $this->hasMany(Rental::class, 'gear_id');
+    }
+}
