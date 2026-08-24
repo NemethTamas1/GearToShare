3ef98ead528ff521f5da27f6df80a0079fa7922f
NemethTamas1 <nemethtamas029@gmail.com>
2026-08-24 12:59:53 +0200

    Gear Request Updated, Documentations Updated

--- backend/app/Http/Requests/UpdateGearRequest.php
+++ backend/app/Http/Requests/UpdateGearRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateGearRequest extends FormRequest
 {
@@ -29,6 +30,49 @@ class UpdateGearRequest extends FormRequest
             "price_per_day" => ["decimal", "required", "numeric", "min:0", "decimal:2"],
             "city" => ["string", "max:100", "min:0", "required"],
             "address" => ["string", "max:255", "min:0", "required"],
+
+            "attributes.battery_capacity_mAh" => [
+                "prohibited_unless:category,power_tool",
+                "required_if:category,power_tool",
+                "integer",
+                "min:0"
+            ],
+
+            "attributes.modes" => [
+                "prohibited_unless:category,power_tool",
+                "array",
+                "min:1",
+            ],
+            "attributes.modes.*" => ["string", Rule::in(["drill", "hammer_drill", "chiseling"])],
+
+            "attributes.load_capacity_kg" => [
+                "prohibited_unless:category,machine",
+                "required_if:category,machine",
+                "numeric",
+                "min:0",
+            ],
+
+            "attributes.fuel_type" => [
+                "prohibited_unless:category,machine",
+                "required_if:category,machine",
+                "string",
+                Rule::in(["benzin", "dizel", "elektromos"]),
+            ],
+
+            "attributes.fuel_tank_l" => [
+                "prohibited_unless:category,machine",
+                "required_if:category,machine",
+                "numeric",
+                "min:0",
+            ],
+
+            "attributes.horsepower" => [
+                "prohibited_unless:category,machine",
+                "required_if:category,machine",
+                "numeric",
+                "min:0",
+            ],
+
         ];
     }
 }
