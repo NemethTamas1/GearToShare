e2055b0cdd497c4d06926f0770bb4f7e74900681
NemethTamas1 <nemethtamas029@gmail.com>
2026-09-03 13:55:27 +0200

    hasfactory added to gear model, also casts added

--- backend/app/Models/Gear.php
+++ backend/app/Models/Gear.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['title', 'description', 'category', 'attributes', 'price_per_day', 'city', 'address'])]
 class Gear extends Model
 {
+    use HasFactory;
+
     protected $table = "gears";
+
+    protected $casts = [
+        'attributes' => 'array',
+    ];
 
     public $timestamps = true;
 
