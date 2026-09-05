d9412e5b54c6490bde30b910faa45ede24f0c95e
NemethTamas1 <nemethtamas029@gmail.com>
2026-09-05 13:01:06 +0200

    Gears endpoint created

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GearController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('/users', UserController::class);
+
+Route::apiResource("/gears", GearController::class);
