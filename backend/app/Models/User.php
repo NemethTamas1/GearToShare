78a66179c8111f93af8a936f75d1bee83e0cf790
NemethTamas1 <nemethtamas029@gmail.com>
2026-09-09 13:57:32 +0200

    User model HasApiTokens added, typo fixed

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -11,12 +11,13 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password', 'phone'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     protected $table = "users";
 
@@ -47,7 +48,7 @@ class User extends Authenticatable
     // Bérbeadott eszközeim.
     public function rentalsAsOwner(): HasManyThrough
     {
-        return $this->hasManyThrough(Renatl::class, Gear::class, 'user_id', 'gear_id');
+        return $this->hasManyThrough(Rental::class, Gear::class, 'user_id', 'gear_id');
     }
 
     // Kiadott értékelési rekordok.
